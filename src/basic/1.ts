da7db46813fc9dbcc7bf8f29ff748bda1ab63aa4
vitalii-22 <smulka69@gmail.com>
2024-04-27 19:24:39 +0300

    Типізував змінні у файлі generics

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -3,8 +3,12 @@ const name: string = "Max";
 const toggle: boolean = true;
 const empty: null = null;
 
-type Callback = (a: number) => number;
+// type Callback = (a: number) => number;
 
-const callback: Callback = (a) => {
+// const callback: Callback = (a) => {
+//   return 100 + a;
+// };
+
+const callback = (a: number): number => {
   return 100 + a;
 };
--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-async function fetchData(url) {
+async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get(url);
+    const response: T = await axios.get(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
